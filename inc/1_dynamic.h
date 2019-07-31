faf68a2f349d828a43b3c77c702db8a4b7e98576
vyshnavc <vyshnavc555@gmail.com>
2019-07-31 12:36:54 +0530

    real number program

--- inc/1_dynamic.h
+++ inc/1_dynamic.h
@@ -1,7 +1,9 @@
+//#include<conio.h>
 #include <unistd.h>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+//#include<Windows.h>
 //#include<time.h>
 //#include <fcntl.h> 
 //#include<signal.h>
