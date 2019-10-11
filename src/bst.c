99a8800202ff2aad15cee32d30628735e39c5aff
vyshnavc <vyshnavc555@gmail.com>
2019-10-11 14:48:00 +0530

    modified spaces

--- src/bst.c
+++ src/bst.c
@@ -1,28 +1,30 @@
 /*Implement a binary seacrh tree to find a particular word from an array of 50 names.*/
+
 #include"../inc/bst.h"
+
 struct node 
 { 
 	char name[50]; 
 	struct node *left, *right; 
 }; 
+
 struct node* insert(struct node*, char *); 
 struct node *newNode(char *); 
 struct node* search(struct node* , char *); 
+
 int main() 
 { 
 	struct node *root = NULL,*searchnode = NULL; 
 	char name[50],option;
-	do
-	{
+
+	do{
 		printf("\n==========BINARY SEARCH TREE==========");
 		printf("\n\nenter a name : ");
 		scanf(" %[^\n]s",name);
-		if(root==NULL)
-		{
+		if(root==NULL){
 			root=insert(root,name);
 			printf("\nname added as root name");
-		}
-		else
+		}else
 		{
 			insert(root,name);
 			printf("\nname added successfully...");
@@ -30,8 +32,7 @@ int main()
 		printf("\ndo you want to add new name y/n? : ");
 		scanf(" %c",&option);
 	}while(option=='y'||option=='Y');
-	do
-	{
+	do{
 		printf("\n==========BINARY SEARCH TREE==========");
 		printf("\n\nenter a name to search : ");
 		scanf(" %[^\n]s",name);
@@ -45,6 +46,7 @@ int main()
 	}while(option=='y'||option=='Y');
 	return 0; 
 }
+
 struct node* insert(struct node* node, char *name) 
 { 
 	if (node == NULL) return newNode(name); 
@@ -55,14 +57,16 @@ struct node* insert(struct node* node, char *name)
 		node->right = insert(node->right,name);    
 
 	return node; 
-} 
+}
+ 
 struct node *newNode(char *name) 
 { 
 	struct node *temp =  (struct node *)malloc(sizeof(struct node)); 
 	strcpy(temp->name,name); 
 	temp->left = temp->right = NULL; 
 	return temp; 
-} 
+}
+ 
 struct node* search(struct node* root,char *name) 
 { 
 	if (root == NULL || strcmp(root->name,name)==0) 
