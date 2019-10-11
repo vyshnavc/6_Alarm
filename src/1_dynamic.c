ea08783a38b7e4fddce55a2730f08e25d54809f8
vyshnavc <vyshnavc555@gmail.com>
2019-10-11 11:36:22 +0530

    modified

--- src/1_dynamic.c
+++ src/1_dynamic.c
@@ -1,88 +1,87 @@
 /* * @file: : Example_1_dynamic.c * * @Brief :write a program to input a list of real numbers of unknown length and output : sum and count of all positive & negative numbers. Perform the * output using following cases * a) Pointer to array * b) Pointer to Pointer * **/
+
 #include"../inc/1_dynamic.h"
+
 int pointerarray(float (*),int);
 int pointerpointer(float **,int);
+
 void main()
 {
 	int nullcheck,size,count;
-        char option;;
+	char option;;
+
 	printf("enter how many real number you want to enter : \n");
 	scanf("%d",&size);
-        float *pointer[size];
-        float *array=malloc(sizeof(float)*size);
+
+	float *pointer[size];
+	float *array=malloc(sizeof(float)*size);
+
 	printf("\n==============================================");
-	for(count=0;count<size;count++)
-	{
+	for(count=0;count<size;count++){
 		printf("\nenter number %d : ",count+1);
 		scanf("%f",&array[count]);
 		pointer[count]=&array[count];
 	}
 	printf("\n==============================================");
-	do
-	{
+	do{
 		printf("\nUsing...?\na)pointer to pointer\nb)pointer to an array\nc)exit program\nenter a option : ");
 		scanf(" %c",&option);
-		if(option=='a'||option=='A')
-		{
-			do
-			{
+		if(option=='a'||option=='A'){
+			do{
 				nullcheck=pointerpointer(pointer,size);
 			}while(nullcheck==0);
 		}
-		else if(option=='b'||option=='B')
-		{
+		else if(option=='b'||option=='B'){
 			do
 			{
 				nullcheck=pointerarray(array,size);
 			}while(nullcheck==0);
 		}
-                else if(option=='c'||option=='C')  
-                return;
-		else
-		{
+		else if(option=='c'||option=='C')  
+			return;
+		else{
 			printf("\nwrong choice");
 		}
 	}while(1);
 }
+
 int pointerpointer(float **array,int size)
 {
-	if(array==NULL)
-	{
+	if(array==NULL){
 		printf("\nsystem error occured ");
 		return 0;
 	}
+
 	int count,postive=0,negative=0;
-        float sum=0;
-	for(count=0;count<size;count++)
-	{
-		if(*array[count]<=-1)
-		{
+	float sum=0;
+
+	for(count=0;count<size;count++){
+		if(*array[count]<=-1){
 			negative++;
-		}
-		else
+		}else
 			postive++;
 		sum=sum+*array[count];
 	}
 	printf("\npostive=%d\nnegative=%d\nsum=%f\n",postive,negative,sum);
 	return 1;
 }
+
 int pointerarray(float (*array),int size)
 {
-	if(array==NULL)
-	{
+	if(array==NULL){
 		printf("\nsystem error occured ");
 		return 0;
 	}
+
 	int count,postive=0,negative=0;
 	float sum=0;
-	for(count=0;count<size;count++)
-	{
+
+	for(count=0;count<size;count++){
 		if(array[count]<=-1)
 			negative++;
 		else
 			postive++;
 		sum=sum+array[count];
-
 	}
 	printf("\npostive=%d\nnegative=%d\nsum=%f\n",postive,negative,sum);
 	return 1;
